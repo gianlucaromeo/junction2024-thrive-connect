0675f39abecbdf47dcc15443159250424cbdd1a7
gianlucaromeo <gianlucaromeo@outlook.com>
2024-11-09 22:22:38 +0200

    Edited start page

--- lib/screens/job_page.dart
+++ lib/screens/job_page.dart
@@ -38,161 +38,180 @@ class JobPage extends GetView<DataController> {
       backgroundColor: AppColors.white,
       body: Padding(
         padding: 24.0.paddingAll,
-        child: Column(
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                /// EMPLOYEES NUMBER
-                Column(
-                  children: [
-                    Text(
-                      "Employees",
-                      style: AppFonts.h2,
-                    ),
-                    Text(
-                      allCompanyEmployees.length.toString(),
-                      style: AppFonts.bodyXL,
-                    ),
-                  ],
-                ),
-
-                /// WELL-BEIGN SCORE
-                Column(
-                  children: [
-                    Text(
-                      "Well-being",
-                      style: AppFonts.h2,
-                    ),
-                    Text(
-                      "67% (89 reviews)",
-                      style: AppFonts.bodyXL,
-                    ),
-                  ],
-                ),
-              ],
-            ),
-            40.0.verticalSpace,
-
-            /// Reviews summary card
-            Container(
-              padding: 20.0.paddingAll,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(12.0),
-                border: Border.all(
-                  color: AppColors.greenPastel,
-                  width: 1.0,
-                ),
-              ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
-                  /// REVIEW QUOTE
-                  Text(
-                    "\"Amazing team-building activities\"",
-                    style: AppFonts.h1.copyWith(
-                      color: AppColors.greenText,
-                    ),
-                  ),
-                  14.0.verticalSpace,
-
-                  /// DESCRIPTION
-                  Text(
-                    "At ${company.name}, we have a lot of fun together, and we believe that it's important to have a good time at work.",
-                    style: AppFonts.bodyM.copyWith(
-                      color: AppColors.greenText,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            20.0.verticalSpace,
-
-            /// JOB POSITION CARD
-            Container(
-              padding: 20.0.paddingAll,
-              decoration: BoxDecoration(
-                color: AppColors.white,
-                borderRadius: BorderRadius.circular(10.0),
-                border: Border.all(
-                  color: AppColors.grey,
-                  width: 1.0,
-                ),
-              ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  /// JOB POSITION
-                  Text(
-                    jobOffer.title,
-                    style: AppFonts.h1,
-                  ),
-                  Divider(
-                    color: AppColors.grey,
-                    thickness: 1.0,
-                    height: 48.0,
-                  ),
-
-                  /// LANGUAGE, TEAM SIZE, AND LOCATION
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  /// EMPLOYEES NUMBER
+                  Column(
                     children: [
-                      Column(
-                        children: [
-                          Text(
-                            "Language",
-                            style: AppFonts.h4,
-                          ),
-                          4.0.verticalSpace,
-                          Text(
-                            jobOffer.language,
-                            style: AppFonts.bodyM,
-                          ),
-                        ],
+                      Text(
+                        "Employees",
+                        style: AppFonts.h2,
                       ),
-                      Column(
-                        children: [
-                          Text(
-                            "Team size",
-                            style: AppFonts.h4,
-                          ),
-                          4.0.verticalSpace,
-                          Text(
-                            allCompanyEmployees.length.toString(),
-                            style: AppFonts.bodyM,
-                          ),
-                        ],
-                      ),
-                      Column(
-                        children: [
-                          Text(
-                            "Location",
-                            style: AppFonts.h4,
-                          ),
-                          4.0.verticalSpace,
-                          Text(
-                            jobOffer.location,
-                            style: AppFonts.bodyM,
-                          ),
-                        ],
+                      Text(
+                        allCompanyEmployees.length.toString(),
+                        style: AppFonts.bodyXL,
                       ),
                     ],
                   ),
-                  Divider(
-                    color: AppColors.grey,
-                    thickness: 1.0,
-                    height: 48.0,
-                  ),
 
-                  /// DESCRIPTION
-                  Text(
-                    jobOffer.description,
-                    style: AppFonts.bodyM,
+                  /// WELL-BEIGN SCORE
+                  Column(
+                    children: [
+                      Text(
+                        "Well-being",
+                        style: AppFonts.h2,
+                      ),
+                      Text(
+                        "67% (89 reviews)",
+                        style: AppFonts.bodyXL,
+                      ),
+                    ],
                   ),
                 ],
               ),
-            ),
-          ],
+              40.0.verticalSpace,
+
+              /// Reviews summary card
+              SizedBox(
+                height: 200.0,
+                child: SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Row(
+                    children: List.generate(
+                      3,
+                          (index) {
+                        return Container(
+                          margin: 8.0.paddingOnlyRight,
+                          padding: 24.0.paddingAll,
+                          width: MediaQuery.of(context).size.width - 72.0, // Adjust width here
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(12.0),
+                            color: index.isOdd
+                                ? AppColors.purpleText
+                                : AppColors.orange,
+                          ),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              /// REVIEW QUOTE
+                              Text(
+                                "\"Amazing team-building activities\"",
+                                style: AppFonts.h2.copyWith(
+                                  color: AppColors.white,
+                                  fontSize: 20.0,
+                                ),
+                              ),
+                              SizedBox(height: 14.0), // Replace verticalSpace
+                              /// DESCRIPTION
+                              Text(
+                                "At ${company.name}, we have a lot of fun together, and we believe that it's important to have a good time at work.",
+                                style: AppFonts.bodyM.copyWith(
+                                  color: AppColors.white,
+                                ),
+                              ),
+                            ],
+                          ),
+                        );
+                      },
+                    ),
+                  ),
+                ),
+              ),
+
+              20.0.verticalSpace,
+
+              /// JOB POSITION CARD
+              Container(
+                padding: 24.0.paddingAll,
+                decoration: BoxDecoration(
+                  color: AppColors.greyLight,
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    /// JOB POSITION
+                    Text(
+                      "Job Position",
+                      style: AppFonts.captionM.copyWith(
+                        fontSize: 20.0,
+                      ),
+                    ),
+                    Text(
+                      jobOffer.title,
+                      style: AppFonts.h2,
+                    ),
+                    Divider(
+                      color: AppColors.grey,
+                      thickness: 1.0,
+                      height: 48.0,
+                    ),
+
+                    /// LANGUAGE, TEAM SIZE, AND LOCATION
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Column(
+                          children: [
+                            Text(
+                              "Language",
+                              style: AppFonts.h4,
+                            ),
+                            4.0.verticalSpace,
+                            Text(
+                              jobOffer.language,
+                              style: AppFonts.bodyM,
+                            ),
+                          ],
+                        ),
+                        Column(
+                          children: [
+                            Text(
+                              "Team size",
+                              style: AppFonts.h4,
+                            ),
+                            4.0.verticalSpace,
+                            Text(
+                              allCompanyEmployees.length.toString(),
+                              style: AppFonts.bodyM,
+                            ),
+                          ],
+                        ),
+                        Column(
+                          children: [
+                            Text(
+                              "Location",
+                              style: AppFonts.h4,
+                            ),
+                            4.0.verticalSpace,
+                            Text(
+                              jobOffer.location,
+                              style: AppFonts.bodyM,
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                    Divider(
+                      color: AppColors.grey,
+                      thickness: 1.0,
+                      height: 48.0,
+                    ),
+
+                    /// DESCRIPTION
+                    Text(
+                      jobOffer.description,
+                      style: AppFonts.bodyM,
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
       floatingActionButton: SizedBox(
